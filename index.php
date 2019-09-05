5512881d1587f859e1dec1b199c18d2f56997522
root <root@diego.diego>
2019-09-05 16:21:44 +0200

    better random

--- index.php
+++ index.php
@@ -1,20 +1,48 @@
 <?php
-	echo shell_exec("wc -l < Perle_di_saggezza");
-    $file = fopen("Perle_di_saggezza","r");
-    $num = rand(0,56);
-    $cont = 0;
-    while(($line = fgets($file)) !== false){
-        if($cont==$num){
+		$found = 0;
+		while($found == 0){
+			$file = fopen("Perle_di_saggezza","r");
+			$lasts = fopen("lasts","r");
+	    $num = rand(0,56);
+	    $cont = 0;
+			$check = 0;
+	    while(($line = fgets($file)) !== false && $check == 0){
+        if($cont == $num){
             $perla = $line;
+						while(($line_lasts = fgets($lasts)) !== false && $check == 0){
+							if($line_lasts == $perla){
+								$check = 1;
+							}
+						}
+						if($check == 0){
+							$found = 1;
+						}
         }
         $cont++;
-    }
-    fclose($file);
+	    }
+			fclose($file);
+			fclose($lasts);
+			$lasts = fopen("lasts","r");
+			$new_text = "";
+			$c = 0;
+			while(($line = fgets($lasts)) !== false){
+				if($c != 0){
+					$new_text .= $line;
+				}
+				$c = 1;
+			}
+			$new_text .= $perla;
+			fclose($lasts);
+			$lasts = fopen("lasts","w");
+			fwrite($lasts, $new_text);
+			fclose($lasts);
+		}
 ?>
 <html>
     <head>
         <title>Perle del Profeta</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Ubuntu&subset=cyrillic,latin' rel='stylesheet' type='text/css'>
         <style>
             body {
                 background-repeat: no-repeat;
