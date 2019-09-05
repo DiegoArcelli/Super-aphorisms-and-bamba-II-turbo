3728d5b07e078a11dfd7e36813ccda31e2a69e73
root <root@diego.diego>
2019-09-05 16:27:19 +0200

    better random

--- index.php
+++ index.php
@@ -1,42 +1,42 @@
 <?php
-		$found = 0;
-		while($found == 0){
-			$file = fopen("Perle_di_saggezza","r");
-			$lasts = fopen("lasts","r");
-	    $num = rand(0,56);
-	    $cont = 0;
-			$check = 0;
-	    while(($line = fgets($file)) !== false && $check == 0){
-        if($cont == $num){
-            $perla = $line;
-						while(($line_lasts = fgets($lasts)) !== false && $check == 0){
-							if($line_lasts == $perla){
-								$check = 1;
-							}
-						}
-						if($check == 0){
-							$found = 1;
-						}
-        }
-        $cont++;
-	    }
-			fclose($file);
-			fclose($lasts);
-			$lasts = fopen("lasts","r");
-			$new_text = "";
-			$c = 0;
-			while(($line = fgets($lasts)) !== false){
-				if($c != 0){
-					$new_text .= $line;
+	$found = 0;
+	while($found == 0){
+		$file = fopen("Perle_di_saggezza","r");
+		$lasts = fopen("lasts","r");
+	    	$num = rand(0,56);
+		$cont = 0;
+		$check = 0;
+	    	while(($line = fgets($file)) !== false && $check == 0){
+        		if($cont == $num){
+            			$perla = $line;
+				while(($line_lasts = fgets($lasts)) !== false && $check == 0){
+					if($line_lasts == $perla){
+						$check = 1;
+					}
 				}
-				$c = 1;
+				if($check == 0){
+					$found = 1;
+				}
+        		}
+        		$cont++;
+	    	}
+		fclose($file);
+		fclose($lasts);
+		$lasts = fopen("lasts","r");
+		$new_text = "";
+		$c = 0;
+		while(($line = fgets($lasts)) !== false){
+			if($c != 0){
+				$new_text .= $line;
 			}
-			$new_text .= $perla;
-			fclose($lasts);
-			$lasts = fopen("lasts","w");
-			fwrite($lasts, $new_text);
-			fclose($lasts);
+			$c = 1;
 		}
+		$new_text .= $perla;
+		fclose($lasts);
+		$lasts = fopen("lasts","w");
+		fwrite($lasts, $new_text);
+		fclose($lasts);
+	}
 ?>
 <html>
     <head>
